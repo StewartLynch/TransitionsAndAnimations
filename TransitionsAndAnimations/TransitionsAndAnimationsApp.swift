f86faf68411377b96e7bdde20ef6391bb0e3266f
Stewart Lynch <slynch@createchsol.com>
2025-02-22 14:36:04 -0800

    Initial Commit

---
+++ TransitionsAndAnimations/TransitionsAndAnimationsApp.swift
@@ -0,0 +1,25 @@
+//
+//----------------------------------------------
+// Original project: TransitionsAndAnimations
+// by  Stewart Lynch on 2025-02-22
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2025 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct TransitionsAndAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
